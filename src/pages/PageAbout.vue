fee339816c93412f365f6f5cf18314c1d77f0f80
Jiseok Oh <jiseok95@gmail.com>
2021-07-13 13:04:23 +0900

    Add: About page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vitae
+        atque mollitia, placeat explicabo exercitationem illum? Eum blanditiis
+        expedita, voluptatibus autem libero itaque officia, nesciunt quis
+        numquam consequuntur dicta? Sit!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vitae
+        atque mollitia, placeat explicabo exercitationem illum? Eum blanditiis
+        expedita, voluptatibus autem libero itaque officia, nesciunt quis
+        numquam consequuntur dicta? Sit!
+      </p>
+    </div>
   </q-page>
 </template>
 
